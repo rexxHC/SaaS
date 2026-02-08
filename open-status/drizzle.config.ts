801da4515dd975e53eee611a496a0979a80bb1f8
rexxHC <rezkrunk@gmail.com>
2026-02-09 01:02:26 +0600

    initialization

---
+++ open-status/drizzle.config.ts
@@ -0,0 +1,13 @@
+import * as dotenv from "dotenv";
+import { defineConfig } from "drizzle-kit";
+
+dotenv.config({ path: ".env.local" });
+
+export default defineConfig({
+  schema: "./lib/db/schema.ts",
+  out: "./lib/db/migrations",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
